6f013c7cf5e4da1e25775a3363bb06b2fd7a2987
DarkSevenX <nargumedor@unicartagena.edu.co>
2025-09-29 15:36:12 -0500

    mejora en los prompts

--- backend/src/prompts/prompts.ts
+++ backend/src/prompts/prompts.ts
@@ -56,10 +56,15 @@ ejemplo:
   - ...item
   - ...item
 
-- conceptos: redactame un apartado de conceptos claves relacionados con los temas los conceptos clave y sus definicones no deben estar divididos por tema, si no que deben ser de todos en general y deben ser solo algunos cada concepto debe tener minimo 40 palabras y el numero de conceptos es maximo 7 y no debes enumearlos, en cambio sus subtitulos los debes poner entre asteriscos (cuida los espacios entre parrafos):
+- conceptos: redactame un apartado de conceptos claves relacionados con los temas los conceptos clave y sus definicones no deben estar divididos por tema, si no que deben ser de todos en general y deben ser solo algunos cada concepto debe tener minimo 40 palabras y el numero de conceptos es maximo 7 y no debes enumearlos, en cambio sus subtitulos los debes poner entre asteriscos:
+
 ejemplo:
   **concepto 1**: definicion, pero puedes usar asteriscos para representar partes en cursiva así *ejemplo*
 
+  **concepto 2**: ... 
+
+  **concepto 2**: ... 
+  
 - resumen: redacta un texto donde hables de el tema o temas. quiero que sean parrafos, donde definas ampliamente cada cosa y hables con soltura el tema o los temas son tambien quiero que te bases en los anteriores objetivos para la redaccion. No mas de 3 parrafos
 
 - metodologia: la metodologia que utilizé para realizar el trabajo. no quiero que digas que use la metodologia de investigacion bibliografica y luego aplicar los conocimientos. se variado debe ser todo un parrafo. evita el uso de las palabras 'exhaustivo' y 'Finalmente' en tu respuesta. quiero que redactes un parrafo donde hables en primera persona y digas que utilizé una metodologia para realizar el protocolo individual sobre el o los temas no quiero que hagas ningun tipo de comentario por tu parte, solamente quiero que me des lo que te pido
